ccb62f5c15884b06db7bd8609281c0214b4a104b
1091-saapp <zengfan3@huawei.com>
2020-09-16 13:25:49 +0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,110 @@
 <template>
   <div class="app">
-    <div id="map" style="height: 100%"></div>
+    <div class="header">MS Intelligent Operation Command Platform</div>
+    <div class="weather">
+      <div class="scroll">
+        <div class="cur">
+          <img class="icon" :src="'https://openweathermap.org/img/wn/'+weather.cur.icon+'@2x.png'" alt="icon">
+          <span class="temp">{{weather.cur.temp}}℃</span>
+          <span class="name">{{weather.cur.name}}</span>
+        </div>
+        <div class="next">
+          <img class="icon" :src="'https://openweathermap.org/img/wn/'+weather.next.icon+'@2x.png'" alt="icon">
+          <span class="temp">{{weather.next.temp}}℃</span>
+          <span class="name">{{weather.next.name}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import mapStyle from "./mapStyle";
 
+let map: google.maps.Map
 export default Vue.extend({
   name: "App",
-  mounted() {
-    new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: {lat: 30, lng: -110},
-        zoom: 8
+  data() {
+    return {
+      map: null,
+      weather: {
+        data: [{
+          icon: "02d",
+          name: "Nairobi",
+          temp: 14
+        }, {
+          icon: "02d",
+          name: "Nairobi",
+          temp: 14
+        }, {
+          icon: "02d",
+          name: "Nairobi",
+          temp: 14
+        }],
+        index: 0,
+        cur: {
+          icon: "02d",
+          name: "Nairobi West",
+          temp: 14
+        },
+        next: {
+          icon: "02d",
+          name: "Nairobi",
+          temp: 14
+        }
+      }
+    }
+  },
+  created() {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: {lat: -1.280682, lng: 36.7685158},
+      zoom: 11,
+      minZoom: 3,
+      maxZoom: 22,
+      styles: mapStyle,
+      mapTypeControl: false,
+      gestureHandling: 'greedy',
+      fullscreenControl: false,
+      zoomControl: false,
+      streetViewControl: false
     });
   }
 })
 </script>
 
-<style lang="scss">
-body{
-  margin: 0;
-  height: 100vh;
-}
-.app{
+<style lang="scss" scoped>
+.app {
   height: 100%;
+}
+
+.header {
+  height: 6rem;
+  background-image: url("/assets/headline.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  opacity: 0.9;
+  font-size: 3rem;
+  line-height: 6rem;
+  color: #54EAB3;
+  text-align: center;
+}
+
+.weather {
+  box-sizing: border-box;
+  position: absolute;
+  height: 6rem;
+  width: 40rem;
+  top: 0;
+  padding: 1rem 2rem;
+  line-height: 4rem;
+  font-size: 1.6rem;
+  word-spacing: 1rem;
+  color: white;
+  overflow: hidden;
+  .icon{
+    height: 4rem;
+    vertical-align: top;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-
-
 import Vue from 'vue'
 import App from './App.vue'
 new Vue({
-    render: createElement => createElement(App)
+    render: createElement => createElement(App),
 }).$mount('#app');
